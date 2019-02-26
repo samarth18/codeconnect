1c49cb2131f9cb99c37b1055b653f6180a7641dc
System Administrator <root@Samarths-MacBook-Pro.local>
2019-02-25 19:12:17 -0500

    Initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://sam:sam@mycluster-pm7pn.mongodb.net/test?retryWrites=true"
+};
---
+++ routes/api/profile.js
@@ -0,0 +1,10 @@
+// Location, Bio, experience, education
+const express = require("express");
+const router = express.Router();
+
+// @route  GET api/profile/test
+// @desc   Tests profile route
+// @access Public
+router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
+
+module.exports = router;
