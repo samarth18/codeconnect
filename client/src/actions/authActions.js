6d8e1e26ab27f5e9be654f4dd2dd2810fab74605
System Administrator <root@Samarths-MBP.hsd1.ma.comcast.net>
2019-03-20 02:15:35 -0400

    Completed entire code

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,58 @@
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+// Register User
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post("/api/users/register", userData)
+    .then(res => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Login - Get a user
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      // Save  web token to local storage
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token); // token will be in string format
+      // Set token to auth header
+      setAuthToken(token);
+      // Get user data from token
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+// Set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
+};
+
+// Log user out
+
+export const logoutUser = () => dispatch => {
+  // Remove token from local storage
+  localStorage.removeItem("jwtToken");
+  // Remove auth header for future requests
+  setAuthToken(false);
+  // Set current user to {} which will set isAuthenticated to false
+  dispatch(setCurrentUser({}));
+};
