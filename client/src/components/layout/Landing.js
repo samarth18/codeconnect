c14e6e282bcb813072ce9862570609777019c4f3
System Administrator <root@Samarths-MBP.hsd1.ma.comcast.net>
2019-03-20 23:55:37 -0400

    All changes to get ready for deployment

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -16,14 +16,11 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Developer Connector</h1>
-                <p className="lead">
-                  {" "}
-                  Create a developer profile/portfolio, share posts and get help
-                  from other developers
-                </p>
+                <h1 className="display-3 mb-4">Code Connect</h1>
+                <h3 className="lead"> Social Network for Developers</h3>
+                <p className="lead"> Create a New Profile</p>
                 <hr />
-                <Link to="/register" className="btn btn-lg btn-info mr-2">
+                <Link to="/register" className="btn btn-lg btn-primary mr-2">
                   Sign Up
                 </Link>
                 <Link to="/login" className="btn btn-lg btn-light">
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -62,10 +62,10 @@ class Navbar extends Component {
     );
 
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+      <nav className="navbar navbar-expand-sm navbar-light bg-light mb-4">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            DevConnector
+            CodeConnect
           </Link>
           <button
             className="navbar-toggler"
